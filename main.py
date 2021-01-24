27a9d105cf9397d492e6cdca548a7b66e935cf75
topcontributor <49335083+topcontributor@users.noreply.github.com>
2021-01-23 19:41:37 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import testFunction
-
-print("this is a test project")
-print("I would like to add this line to the current project.")
-print("I have no clue what I'm doing right now")
 x = testFunction
 x.displaytext()
 
--- testFunction.py
+++ testFunction.py
@@ -1,3 +1,5 @@
 def displaytext():
+    print("this is a test project")
+    print("I would like to add this line to the current project.")
+    print("I have no clue what I'm doing right now")
     print("this is coming from another file")
-
