5d1d96f9109321fbb65c5d1cf401393f972dcc5f
topcontributor <49335083+topcontributor@users.noreply.github.com>
2021-01-23 19:39:05 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import testFunction
+
 print("this is a test project")
 print("I would like to add this line to the current project.")
-print("I have no idea what I am doing right now")
-print("I finally understand, you create a new branch, than the owner creates a pull request merging the branch into main")
+print("I have no clue what I'm doing right now")
+x = testFunction
+x.displaytext()
+
+
---
+++ testFunction.py
@@ -0,0 +1,3 @@
+def displaytext():
+    print("this is coming from another file")
+
